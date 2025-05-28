4022b94e678c43a4939c80baa78ccbad8c29c058
cgarciazavaleta <cgarciazavaleta@udesa.edu.ar>
2025-05-28 17:43:43 -0300

    intentamos empezar

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let mammamiadet = document.querySelector(".peliculasmovie");
+let mammamia = document.querySelector(".mammamia");
+
+mammamia.addEventListener("click", function(){
+    mammamiadet.style.display = "none";
+})
