ae79eb5289895ca779cf6970165580d81316f880
TDietrichOxenford <tdietrichoxenford@udesa.edu.ar>
2025-06-03 18:10:15 -0300

    primer intento de cogido js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-let mammamiadet = document.querySelector(".peliculasmovie");
-let mammamia = document.querySelector(".mammamia");
+let peliculas = document.querySelector(".seccionuno")
+let characters = ""
 
-mammamia.addEventListener("click", function(){
-    mammamiadet.style.display = "none";
+fetch("https://api.themoviedb.org/3/account/49123d0deec6cb1469f51f0da1c92172/lists")
+.then(function(response) {
+    return response.json()
 })
+.then(function(data){
+    let results = data.results;
+    console.log(data)
+
+}) 
