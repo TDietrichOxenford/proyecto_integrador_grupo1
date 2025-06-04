656bc7346413a3bd4be5aa6b79a7599903dd9d14
cgarciazavaleta <cgarciazavaleta@udesa.edu.ar>
2025-06-04 15:02:25 -0300

    js

--- js/index.js
+++ js/index.js
@@ -31,3 +31,31 @@ fetch(url)
   });
 
 
+let info = document.querySelector(".secciondos"); 
+let url2 = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`;
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let movies = data.results; 
+
+    for (let i = 0; i < 5; i++) {
+      moviesHTML += `
+        <a class="linkdetail" href="./detail-movie.html?id=${movies[i].id}">
+          <div class="peliculas">
+            <img class="imgpelis" src="https://image.tmdb.org/t/p/w500${movies[i].poster_path}" alt="${movies[i].title}">
+            <p class="textseccion">
+              <span class="negrita">${movies[i].title}</span> (${movies[i].release_date})
+            </p>
+          </div>
+        </a>
+      `;
+    }
+
+    info.innerHTML = moviesHTML;
+  })
+  .catch(function(error) {
+    console.error("Ocurrió un error:", error.message);
+  });
