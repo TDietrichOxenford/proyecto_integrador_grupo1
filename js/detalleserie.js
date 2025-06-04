3d0fb69b1d047fcf047d03984411e616c7780163
cgarciazavaleta <cgarciazavaleta@udesa.edu.ar>
2025-06-04 17:22:00 -0300

    detalleserie.js

---
+++ js/detalleserie.js
@@ -0,0 +1,37 @@
+let queryString = location.search;
+let queryserie = new URLSearchParams(queryString)
+let id = queryserie.get("id")
+let serie = document.querySelector(".peliculasmovie")
+let apiKey = "49123d0deec6cb1469f51f0da1c92172";
+
+console.log("id")
+
+fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`)
+
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+   console.log(data)
+    serie.innerHTML = `
+            <div class="partesmovie cajauno">
+            <img class="fotouno" src="${data.poster_path}" alt="${data.title}">
+        </div>
+        <div class="partesmovie cajados">
+            <h3>${data.title}</h3>
+            <p><span class="negrita">Calificación:</span> ${data.popularity}</p>
+            <p><span class="negrita">Fecha de estreno:</span> ${data.release_date}</p>
+        </div>
+        <div class="partesmovie cajatres cajatresseries">
+            <p><span class="negrita">Duración:</span> ${data.runtime}</p>
+            <a href="./seriesgenres.html"><p><span class="negrita">Género:</span> </p></a>
+        </div>
+        <div class="partesmovie cajacuatro">
+            <p><span class="negrita">SINOPSIS:</span> ${data.overview} </p>
+        </div>
+ 
+        `
+})
+.catch(function(error) {
+    console.error("Ocurrió un error:", error.message);
+  });
