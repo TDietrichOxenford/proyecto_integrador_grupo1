7d6b060b93b271d1a9ac2bd50b298b50d0ebe7a0
cgarciazavaleta <cgarciazavaleta@udesa.edu.ar>
2025-06-06 09:22:59 -0300

    resultados.js

--- js/resultadosmovie.js
+++ js/resultadosmovie.js
@@ -2,7 +2,7 @@ let queryString = location.search;
 let queryserie = new URLSearchParams(queryString)
 let id = queryserie.get("buscar")
 let apiKey = "49123d0deec6cb1469f51f0da1c92172";
-let info = document.querySelector(".results"); 
+let info = document.querySelector(".seccionuno"); 
 let resultsHTML = ""
 
 let url = `https://api.themoviedb.org/3/${id}/popular?api_key=${apiKey}&language=es-ES&page=1`;
@@ -13,14 +13,14 @@ fetch(url)
   })
   .then(function(data) {
     let movies = data.results; 
-
+console.log(movies)
     for (let i = 0; i < 5; i++) {
       resultsHTML += `
        <a class="linkdetail" href="./detail-movie.html?id=${movies[i].id}">
           <div class="peliculas">
             <img class="imgpelis" src="https://image.tmdb.org/t/p/w500${movies[i].poster_path}" alt="${movies[i].title}">
             <p class="textseccion">
-              <span class="negrita">${movies[i].title}</span> (${movies[i].release_date})
+              <span class="negrita">${movies[i].original_title}</span> (${movies[i].release_date})
             </p>
           </div>
         </a>
@@ -34,7 +34,8 @@ fetch(url)
     console.error("Ocurrió un error:", error.message);
   });
 
-let infoserie = document.querySelector(".results"); 
+
+let infoserie = document.querySelector(".seccionuno"); 
 let resultsserieHTML = ""
 let urldos = `https://api.themoviedb.org/3/${id}/popular?api_key=${apiKey}`;
 
@@ -44,14 +45,14 @@ fetch(urldos)
   })
   .then(function(data) {
     let serie = data.results; 
-
+console.log(serie)
     for (let i = 0; i < 5; i++) {
       resultsserieHTML += `
         <a class="linkdetail" href="./detail-serie.html?id=${serie[i].id}">
           <div class="peliculas">
             <img class="imgpelis" src="https://image.tmdb.org/t/p/w500${serie[i].poster_path}" alt="${serie[i].name}">
             <p class="textseccion">
-              <span class="negrita">${serie[i].name}</span> (${serie[i].first_air_date})
+              <span class="negrita">${serie[i].original_name}</span> (${serie[i].first_air_date})
             </p>
           </div>
         </a>
